6363ec62ad5f3be224c0a32d1c64bca75691691f
Bohdan Kibkalo <bohdan.kibkalo@gmail.com>
2023-10-10 14:45:53 +0200

    merge with main

--- inventory-service/src/test/java/com/kibikalo/inventoryservice/controller/InventoryControllerIntegrationTests.java
+++ inventory-service/src/test/java/com/kibikalo/inventoryservice/controller/InventoryControllerIntegrationTests.java
@@ -2,15 +2,8 @@ package com.kibikalo.inventoryservice.controller;
 
 
 import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 
@@ -19,27 +12,4 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 @Testcontainers
 @Slf4j
 public class InventoryControllerIntegrationTests {
-
-    @BeforeAll
-    public static void beforeAll() {
-        PostgreSQLContainer<?> postgresContainer = new PostgreSQLContainer<>("postgres:13")
-                .withDatabaseName("inventory-service")
-                .withUsername("postgres")
-                .withPassword("postgres");
-
-        postgresContainer.start();
-
-        System.setProperty("spring.datasource.url", postgresContainer.getJdbcUrl());
-        System.setProperty("spring.datasource.username", postgresContainer.getUsername());
-        System.setProperty("spring.datasource.password", postgresContainer.getPassword());
-    }
-
-    @Autowired
-    private MockMvc mockMvc;
-
-    @Test
-    void shouldCheckIsInStock() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.get("/api/inventory/"))
-                .andExpect(MockMvcResultMatchers.status().isOk());
-    }
 }
